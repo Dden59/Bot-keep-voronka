77c754abe30fd390676b2a0042df5756b908a4c7
Dden59 <prmden90@gmail.com>
2026-01-18 10:49:58 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "7412062591:AAE1zsY47ijdbHEE7vlOLeGBAFeUb5ks384"  # токен бота
+CHANNEL_URL = "https://t.me/+A3VYdI2a4yIyYzJi"  # Ссылка на канал (проверка сабки)
+CHANNEL_ID = "-1002610974494"  # id канала. Начинаеться с минуса (проверка сабки)
+VERIF_CHANNEL_ID = "-1002300299698"  # айди канала для верификации айдишников, начинается с минуса (чат с айди)
+SUPP = "https://t.me/D_den" # хз ваще бы вырезал
+ADMIN_ID = 275011097 #свой айди 
+PROMO = "JetJet500"
